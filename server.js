3d225f2ec0e832d3e274bd6317fd541943f81506
vayne <miguel.osorio19@yahoo.com>
2022-01-20 14:33:07 -0600

    added frontend functions

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-// const e = require('express');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
-const { type } = require('os');
 const path = require('path');
-const { off } = require('process');
+const { animals } = require('./data/animals');
 // parse incoming string or array data - is a method built into Express.js. It takes incoming POST data and converts it to key/value pairings that can be accessed in the req.body object.
 // The extended: true option set inside the method call informs our server that there may be sub-array data nested in it as well, so it needs to look as deep into the POST data as possible to parse all of the data correctly.
 app.use(express.urlencoded({ extended: true}));
 // parse incoming JSON data - takes incoming POST data in the form of JSON and parses it into the req.body JavaScript object
 app.use(express.json());
-
-const { animals } = require('./data/animals');
+// The way it works is that we provide a file path to a location in our application (in this case, the public folder) and instruct the server to make these files static resources.
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -102,7 +100,7 @@ app.post('/api/animals', (req, res) => {
     if(!validateAnimal(req.body)) {
         res.status(400).send('THe animal is not properly formatted.');
     } else {
-        
+
     }
 
     // add animal to json file and animals array in this function
@@ -130,6 +128,23 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+// The * will act as a wildcard, meaning any route that wasn't previously defined will fall under this request and will receive the homepage as the response. 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
